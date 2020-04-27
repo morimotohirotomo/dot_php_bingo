7ae7d32daf57ee323abac402afbf4ddeabe1a389
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-27 21:49:38 +0900

    add

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+  $nums = [];
+
+  for($i = 0; $i < 5; $i++) {
+  $col = range($i * 15 + 1, $i * 15 + 15);
+  shuffle($col);
+  $nums[$i] = array_slice($col, 0, 5);
+}
+
+print_r($nums);
+exit;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
