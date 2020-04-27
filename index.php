baa1813b61c714c41396a5182ea04c39f3981c09
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-27 21:40:15 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>BINGO</title>
+  <link rel="stylesheet" type="" href="styles.css">
+</head>
+<body>
+  <div id="container">
+  	<table>
+  	  <tr>
+  	  	<th>B</th>
+  	  	<th>I</th>
+  	  	<th>N</th>
+  	  	<th>G</th>
+  	  	<th>O</th>
+  	  </tr>
+  	  <tr>
+  	  	<td>1</td>
+  	  	<td>1</td>
+  	  	<td>1</td>
+  	  	<td>1</td>
+  	  	<td>1</td>
+  	  </tr>
+  	</table>
+  </div>
+</body>
+</html>
